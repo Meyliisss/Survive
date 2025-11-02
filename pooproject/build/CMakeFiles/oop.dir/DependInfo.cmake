0604f2f99b02d6d361e99a8ff330ea8f87e5e815
Meyliisss <meylishudayberdiyew05@gmail.com>
2025-11-02 23:40:17 +0200

    Add files via upload

---
+++ pooproject/build/CMakeFiles/oop.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/meylishudayberdiyev/Desktop/pooproject/src/game.cpp" "CMakeFiles/oop.dir/src/game.cpp.o" "gcc" "CMakeFiles/oop.dir/src/game.cpp.o.d"
+  "/Users/meylishudayberdiyev/Desktop/pooproject/src/item.cpp" "CMakeFiles/oop.dir/src/item.cpp.o" "gcc" "CMakeFiles/oop.dir/src/item.cpp.o.d"
+  "/Users/meylishudayberdiyev/Desktop/pooproject/src/main.cpp" "CMakeFiles/oop.dir/src/main.cpp.o" "gcc" "CMakeFiles/oop.dir/src/main.cpp.o.d"
+  "/Users/meylishudayberdiyev/Desktop/pooproject/src/player.cpp" "CMakeFiles/oop.dir/src/player.cpp.o" "gcc" "CMakeFiles/oop.dir/src/player.cpp.o.d"
+  "/Users/meylishudayberdiyev/Desktop/pooproject/src/world.cpp" "CMakeFiles/oop.dir/src/world.cpp.o" "gcc" "CMakeFiles/oop.dir/src/world.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
